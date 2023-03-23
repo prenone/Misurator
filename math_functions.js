cc74100c9ee4c509380c30d75c212e6cd83365bb
Achille Merendino <achille@achilleme.com>
2023-03-23 09:40:59 +0000

    Fixed average value of empty array

--- math_functions.js
+++ math_functions.js
@@ -2,5 +2,5 @@ import { measurementsToArray } from "./exportdata_generators.js";
 
 export function calculateAverage(measurements) {
     const measures = measurementsToArray(measurements);
-    return measures.reduce((acc, val) => acc + val) / measurements.length
+    return measures.reduce((acc, val) => acc + val, 0) / measurements.length;
 }
